8e50182b77ea3ad6826ea2774b25091cb34e755a
Psyhogenesis <psyhogenesis@gmail.com>
2014-11-02 16:12:50 +0100

    Zadefiniowano nowa ciale klasy

--- src/Player.java
+++ src/Player.java
@@ -1,11 +1,11 @@
 
 public class Player {
 	private String name;
-
+	//metoda ustawia zmienna name zadanym stringiem
 	public void setName(String p_name){
 		this.name = p_name;
 	}
-
+	//metoda zwraca zmienna name
     public String getName() {
     	return this.name;
     }
