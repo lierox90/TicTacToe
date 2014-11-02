425eec0bb2c054df69f38762bed4401bdca0e8a3
Psyhogenesis <psyhogenesis@gmail.com>
2014-11-02 16:14:49 +0100

    Stworzona nowa klase Tile dla sprawdzenia czy jest pole zajete czy jest puste.

---
+++ src/Tile.java
@@ -0,0 +1,20 @@
+
+public class Tile {
+    private Player player = null;
+    
+    //metoda ustawia zmienna player zadanej zmiennej
+    public void fill(Player p_player) {
+        this.player = p_player;
+    }
+    //metoda sprawdza czy pole nie jest zajete
+    public boolean isMarked() {
+        if (player != null) {
+            return true;
+        }
+        return false;
+    }
+    //metoda zwraca zmienna player
+    public Player getPlayer() {
+        return player;
+    }
+}
