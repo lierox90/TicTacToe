4355c5c32c743260d963e9187a25b9d019dd6614
Paweł Mieleń <lierox90@gmail.com>
2014-11-02 20:59:01 +0100

    Visualisation of Button grid

--- src/GUI.java
+++ src/GUI.java
@@ -8,18 +8,21 @@ import javax.swing.JFrame;
 
 public class GUI extends JFrame 
 {
-	Game game;
-	List<List<JButton>> buttons;
+	private Game game;
+	private List<ArrayList<JButton>> buttons;
+	private int width = 300;
+	private int heigh = width+100;
     public GUI() throws IOException 
     {
     	//Window init
         super("TicTacToe");
-        setSize(800, 600);
+        setSize(width, heigh);
         setResizable( false );
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
         setLayout(null);
         //Buttons Grid
+        buttons = new ArrayList<ArrayList<JButton>>();
         for(int i=0;i<3;i++)
         {
         	buttons.add(new ArrayList<JButton>());
@@ -28,10 +31,34 @@ public class GUI extends JFrame
             	buttons.get(i).add(new JButton());
             }
         }
+        setButtons();
         //Game object
         game = new Game();
         // Main addition
+        for(int i=0;i<3;i++)
+        {
+            for(int j=0;j<3;j++)
+            {
+            	this.add(buttons.get(i).get(j));
+            }
+        }
         this.setBackground(new Color(255,255,255));
+    }
+    void setButtons()
+    {
+    	int step = (width-6)/3;
+    	int initH = heigh-width-23;
+    	int initW = 0;
+        for(int i=0;i<3;i++)
+        {
+            for(int j=0;j<3;j++)
+            {
+            	buttons.get(i).get(j).setBounds(initW, initH, step, step);
+            	initW += step;
+            }
+            initW = 0;
+            initH += step;
+        }
     }
 }
 
